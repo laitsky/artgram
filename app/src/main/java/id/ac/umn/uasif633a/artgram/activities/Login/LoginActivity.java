f9cf71b4f93b5254776c3cec5b0f893eac500c82
VINCENT DIAMOND . (00000027047) <vincent12@student.umn.ac.id>
2021-04-02 10:48:31 +0700

    first commit

---
+++ app/src/main/java/id/ac/umn/uasif633a/artgram/activities/Login/LoginActivity.java
@@ -0,0 +1,16 @@
+package id.ac.umn.uasif633a.artgram.activities.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.ac.umn.uasif633a.artgram.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/id/ac/umn/uasif633a/artgram/activities/MainPage/MainPage.java
@@ -0,0 +1,16 @@
+package id.ac.umn.uasif633a.artgram.activities.MainPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.ac.umn.uasif633a.artgram.R;
+
+public class MainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+}
